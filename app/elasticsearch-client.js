c445836dc69fb9fdc580c7c652cbe3205eb4f00f
gnocchiz <lukaribarola@gmail.com>
2024-08-01 15:34:32 +0200

    dsa

--- app/elasticsearch-client.js
+++
@@ -1,6 +0,0 @@
-// elasticsearch-client.js
-const { Client } = require('@elastic/elasticsearch');
-
-const client = new Client({ node: 'http://localhost:9200' }); // Asegúrate de que Elasticsearch esté ejecutándose en esta URL
-
-module.exports = client;
